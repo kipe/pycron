64d3938b00519baf3598c9a53754f46f17d14a50
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-06-18 17:52:53 +0300

    Remove useless checking of 'every' argument, it's users own stupidity if the value is out of range or could be inserted more easily.

--- pycron/__init__.py
+++ pycron/__init__.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def _parse_arg(value, target, maximum):
+def _parse_arg(value, target):
     if value == '*':
         return True
 
@@ -18,11 +18,6 @@ def _parse_arg(value, target, maximum):
         value, interval = value.split('/')
         if value != '*':
             raise ValueError
-
-        interval = int(interval)
-        if interval not in range(0, maximum + 1):
-            raise ValueError
-
         return target % int(interval) == 0
 
     if int(value) == target:
@@ -40,8 +35,8 @@ def is_now(s):
     now = datetime.now()
     minute, hour, dom, month, dow = s.split(' ')
 
-    return _parse_arg(minute, now.minute, 30) \
-        and _parse_arg(hour, now.hour, 12) \
-        and _parse_arg(dom, now.day, 14) \
-        and _parse_arg(month, now.month, 6) \
-        and _parse_arg(dow, now.weekday(), 3)
+    return _parse_arg(minute, now.minute) \
+        and _parse_arg(hour, now.hour) \
+        and _parse_arg(dom, now.day) \
+        and _parse_arg(month, now.month) \
+        and _parse_arg(dow, now.weekday())
--- tests/mock.py
+++ tests/mock.py
@@ -9,8 +9,8 @@ def is_now(now, s):
     '''
     minute, hour, dom, month, dow = s.split(' ')
 
-    return _parse_arg(minute, now.minute, 30) \
-        and _parse_arg(hour, now.hour, 12) \
-        and _parse_arg(dom, now.day, 14) \
-        and _parse_arg(month, now.month, 6) \
-        and _parse_arg(dow, now.weekday(), 3)
+    return _parse_arg(minute, now.minute) \
+        and _parse_arg(hour, now.hour) \
+        and _parse_arg(dom, now.day) \
+        and _parse_arg(month, now.month) \
+        and _parse_arg(dow, now.weekday())
