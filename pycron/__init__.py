b5fc25884e659dedc49806bbf154b64a125ed70e
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-11-30 12:46:12 +0200

    Allow setting of date to is_now()

--- pycron/__init__.py
+++ pycron/__init__.py
@@ -35,17 +35,21 @@ def _parse_arg(value, target):
     return False
 
 
-def is_now(s):
+def is_now(s, dt=None):
     '''
     A very simple cron-like parser to determine, if (cron-like) string is valid for this date and time.
-    @input: cron-like string (minute, hour, day of month, month, day of week)
+    @input:
+        s = cron-like string (minute, hour, day of month, month, day of week)
+        dt = datetime to use as reference time, defaults to now
     @output: boolean of result
     '''
-    now = datetime.now()
+    if dt is None:
+        dt = datetime.now()
     minute, hour, dom, month, dow = s.split(' ')
+    weekday = dt.isoweekday()
 
-    return _parse_arg(minute, now.minute) \
-        and _parse_arg(hour, now.hour) \
-        and _parse_arg(dom, now.day) \
-        and _parse_arg(month, now.month) \
-        and _parse_arg(dow, now.weekday())
+    return _parse_arg(minute, dt.minute) \
+        and _parse_arg(hour, dt.hour) \
+        and _parse_arg(dom, dt.day) \
+        and _parse_arg(month, dt.month) \
+        and _parse_arg(dow, 0 if weekday == 7 else weekday)
