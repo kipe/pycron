e7d42847284ae73befad8bdf2fa035a6f95a82bb
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-12-01 16:36:26 +0200

    Add more thorough testing of day of week.

--- tests/test_dow.py
+++ tests/test_dow.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pycron
 
 
@@ -14,5 +14,12 @@ def test_parser():
     assert pycron.DOW_CHOICES[now.isoweekday()][1] == 'Thursday'
     assert pycron.DOW_CHOICES[0][1] == 'Sunday'
 
-    now = datetime(2015, 6, 21, 16, 7)
-    assert pycron.is_now('* * * * 0', now)
+    now = datetime(2015, 6, 20, 16, 7)
+    for i in range(0, 7):
+        # Test day matching from Sunday onwards...
+        now += timedelta(days=1)
+        assert pycron.is_now('* * * * %i' % (i), now)
+        # Test weekdays
+        assert pycron.is_now('* * * * 1,2,3,4,5', now) is (True if i not in [0, 6] else False)
+        # Test weekends
+        assert pycron.is_now('* * * * 0,6', now) is (True if i in [0, 6] else False)
