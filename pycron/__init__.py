c7992998245ee1ef148fcbb493d5ea83eca0bdc3
Kimmo Huoman <kipenroskaposti@gmail.com>
2017-12-30 17:48:34 +0200

    Add test-requirements.txt, install it in travis.yml

--- pycron/__init__.py
+++ pycron/__init__.py
@@ -69,7 +69,7 @@ def is_now(s, dt=None):
 
 def has_been(s, since, dt=None):
     '''
-    A parser to check whether a (cron-like) string has been true during a certain time period. 
+    A parser to check whether a (cron-like) string has been true during a certain time period.
     Useful for applications which cannot check every minute or need to catch up during a restart.
     @input:
         s = cron-like string (minute, hour, day of month, month, day of week)
@@ -87,5 +87,5 @@ def has_been(s, since, dt=None):
         if is_now(s, since):
             return True
         since += timedelta(minutes=1)
-    
+
     return False
